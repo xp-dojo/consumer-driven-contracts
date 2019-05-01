15ac5d7024505ab4d6791b4c0d44d8b9e909d499
Peter Suggitt <me@suggs.org.uk>
2019-05-01 19:45:37 +0100

    added a discovery service

---
+++ discovery-service/README.md
@@ -0,0 +1,9 @@
+# Discovery Service
+This service is an instance of a Eureka discovery service.  Services register with this discovery service so they can 
+find them based on a n alias name.
+
+To start this service run it from inside the IDE (right click on the main method and run it) or run it as a jat from the command line: with `java -jar `
+
+Once it is running you can find it at [http://localhost:8761](http://localhost:8761).
+
+You can look at the client to see which services are registered.
---
+++ discovery-service/src/main/java/org/xpdojo/bank/cdc/discovery/DiscoveryService.java
@@ -0,0 +1,35 @@
+package org.xpdojo.bank.cdc.discovery;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static org.springframework.boot.WebApplicationType.SERVLET;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableSwagger2
+public class DiscoveryService {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(DiscoveryService.class)
+                .web(SERVLET)
+                .run(args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
