083602e37a58813ab8a9ef92d8e3d2748ff7dbac
Peter Suggitt <me@suggs.org.uk>
2019-05-06 09:28:23 +0100

    created other two services in skaleton

---
+++ README.md
@@ -0,0 +1,8 @@
+##
+
+
+
+### Notes
+Things to draw out in the presso:
+ - services are not compiled against other services.  We only know if they work at runtime.
+ - contracts allow us to be clear about the data in the payloads, as defined by the consumers
---
+++ mobile-banking-service/src/main/java/org/xpdojo/bank/cdc/mobile/MobileBankingService.java
@@ -0,0 +1,34 @@
+package org.xpdojo.bank.cdc.mobile;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static org.springframework.boot.WebApplicationType.SERVLET;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableSwagger2
+public class MobileBankingService {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MobileBankingService.class)
+                .web(SERVLET)
+                .run(args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
