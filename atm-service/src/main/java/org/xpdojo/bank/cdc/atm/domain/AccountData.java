d20af7449640d8022e9d9a3731bf220fe2313796
Peter Suggitt <me@suggs.org.uk>
2019-06-10 07:39:17 +0100

    fixed a silly bug

--- atm-service/src/main/java/org/xpdojo/bank/cdc/atm/domain/AccountData.java
+++ atm-service/src/main/java/org/xpdojo/bank/cdc/atm/domain/AccountData.java
@@ -10,16 +10,16 @@ import java.util.Objects;
 public class AccountData {
 
     private final Long accountNumber;
-    private final String accountDescription;
+    private final String description;
     private final Double overdraftFacility;
     private final Double balance;
 
     public AccountData(@JsonProperty("accountNumber") final Long accountNumber,
-                       @JsonProperty("description") final String accountDescription,
+                       @JsonProperty("description") final String description,
                        @JsonProperty("overdraftFacility") final Amount overdraftFacility,
                        @JsonProperty("balance") final Amount balance) {
         this.accountNumber = accountNumber;
-        this.accountDescription = accountDescription;
+        this.description = description;
         this.overdraftFacility = overdraftFacility.getValue();
         this.balance = balance.getValue();
     }
@@ -28,8 +28,8 @@ public class AccountData {
         return accountNumber;
     }
 
-    public String getAccountDescription() {
-        return accountDescription;
+    public String getDescription() {
+        return description;
     }
 
     public Double getOverdraftFacility() {
@@ -46,21 +46,21 @@ public class AccountData {
         if (o == null || getClass() != o.getClass()) return false;
         AccountData that = (AccountData) o;
         return Objects.equals(accountNumber, that.accountNumber) &&
-                Objects.equals(accountDescription, that.accountDescription) &&
+                Objects.equals(description, that.description) &&
                 Objects.equals(overdraftFacility, that.overdraftFacility) &&
                 Objects.equals(balance, that.balance);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(accountNumber, accountDescription, overdraftFacility, balance);
+        return Objects.hash(accountNumber, description, overdraftFacility, balance);
     }
 
     @Override
     public String toString() {
         return "AccountData{" +
                 "accountNumber=" + accountNumber +
-                ", accountDescription='" + accountDescription + '\'' +
+                ", description='" + description + '\'' +
                 ", overdraftFacility=" + overdraftFacility +
                 ", balance=" + balance +
                 '}';
