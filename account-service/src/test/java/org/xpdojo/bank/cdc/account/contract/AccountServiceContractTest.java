357e2161878ac23cff5e52ad2fb104590158f170
Peter Suggitt <me@suggs.org.uk>
2026-01-27 13:01:18 +0000

    upgraded pact

--- account-service/src/test/java/org/xpdojo/bank/cdc/account/contract/AccountServiceContractTest.java
+++ account-service/src/test/java/org/xpdojo/bank/cdc/account/contract/AccountServiceContractTest.java
@@ -1,11 +1,11 @@
 package org.xpdojo.bank.cdc.account.contract;
 
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
